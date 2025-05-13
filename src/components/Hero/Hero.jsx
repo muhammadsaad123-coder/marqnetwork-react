d8e26ef672bc05d90558caa4a5a50abfa4af5c7f
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-05-14 02:03:33 +0500

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,32 @@
 import React from "react";
-import './Hero.css'
+import './Hero.css';
 
-const Hero =() => {
-    return(
-        <section className="hero" >
+const Hero = () => {
+  return (
+    <section className="hero">
 
-<div className="hero__content">
-        {/* Gradient large heading */}
+      <img src='images/animation.gif' alt="Animated Background" className="hero__background-svg" />
+ 
+
+
+      <div className="hero__content">
         <h1>
-          Premium <span> Agency</span><br />
+          Premium <span>Agency</span><br />
           for <span>Creatives.</span>
         </h1>
 
-        {/* Subtitle paragraph */}
         <p>
           We specialize in crafting unique digital presence<br />
           that help businesses grow and stand out in their industries.
         </p>
 
-        {/* Call to action buttons */}
         <div className="hero__buttons">
           <button className="primary-btn">Connect With Us</button>
           <button className="secondary-btn">What is Landin?</button>
         </div>
       </div>
-
-         
-
-        </section>
-    )
+    </section>
+  );
 };
 
 export default Hero;
